3981c5d1699e1182b373b85f43a84c7a40292371
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-07-13 16:08:42 +0800

    feat: add user seeder

---
+++ models/seeds/userSeeder.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const User = require('../user') // 載入 user model
+
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
+mongoose.connect('mongodb://localhost/users', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  User.create(users)
+  console.log('done')
+})
--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema
 const userSchema = new Schema({
   firstName: {
     type: String,
+    required: true
   },
   email: {
     type: String,
