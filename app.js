2cf49165924649ecf6a7a299508abb82922cd71e
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-07-13 17:59:42 +0800

    feat: add login function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const User = require('./models/user')
 
 const app = express()
 const port = 3000
@@ -18,10 +21,27 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// routes
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body
+
+  User.find({ email, password })
+    .lean()
+    .then(user => {
+      if (user.length === 1) {
+        res.render('welcome', { name: user[0].firstName })
+      }
+      res.render('login', { loginFail: "true", email })
+    })
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
-  console.log(`app is running on http://localhost:${port}`)
+  console.log(`app is running on http://localhost:${port}/login`)
 })
